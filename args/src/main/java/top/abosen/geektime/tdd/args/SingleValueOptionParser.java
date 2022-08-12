3bf256299b6ff00de46029ab3615684cc02d5156
qiubaisen <abosen@qq.com>
2022-08-12 16:34:31 +0800

    refactor: 提取方法,通过方法名表现意图

--- args/src/main/java/top/abosen/geektime/tdd/args/SingleValueOptionParser.java
+++ args/src/main/java/top/abosen/geektime/tdd/args/SingleValueOptionParser.java
@@ -16,13 +16,25 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (index + 1 >= arguments.size() || arguments.get(index + 1).startsWith("-")) {
+        if (isReachEndOfList(arguments, index) || isFollowedByOtherFlag(arguments, index)) {
             throw new InsufficientArgumentsException(option.value());
         }
-        if (index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-")) {
+        if (secondArgumentIsNotFlag(arguments, index)) {
             throw new TooManyArgumentsException(option.value());
         }
         String value = arguments.get(index + 1);
         return valueParser.apply(value);
+    }
+
+    private static boolean secondArgumentIsNotFlag(List<String> arguments, int index) {
+        return index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-");
+    }
+
+    private static boolean isFollowedByOtherFlag(List<String> arguments, int index) {
+        return arguments.get(index + 1).startsWith("-");
+    }
+
+    private static boolean isReachEndOfList(List<String> arguments, int index) {
+        return index + 1 >= arguments.size();
     }
 }
