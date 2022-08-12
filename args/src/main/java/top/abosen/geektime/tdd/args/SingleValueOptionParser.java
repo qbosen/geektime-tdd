4e4c94e75fe627dbe6043fec5474585657c8b0a3
qiubaisen <abosen@qq.com>
2022-08-12 16:43:33 +0800

    refactor: 通过提取接下来的参数列表来优化校验意图

--- args/src/main/java/top/abosen/geektime/tdd/args/SingleValueOptionParser.java
+++ args/src/main/java/top/abosen/geektime/tdd/args/SingleValueOptionParser.java
@@ -2,6 +2,7 @@ package top.abosen.geektime.tdd.args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingleValueOptionParser<T> implements OptionParser<T> {
     T defaultValue;
@@ -16,25 +17,23 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (isReachEndOfList(arguments, index) || isFollowedByOtherFlag(arguments, index)) {
+
+        List<String> values = values(arguments, index);
+
+        if (values.size() < 1) {
             throw new InsufficientArgumentsException(option.value());
         }
-        if (secondArgumentIsNotFlag(arguments, index)) {
+        if (values.size() > 1) {
             throw new TooManyArgumentsException(option.value());
         }
         String value = arguments.get(index + 1);
         return valueParser.apply(value);
     }
 
-    private static boolean secondArgumentIsNotFlag(List<String> arguments, int index) {
-        return index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-");
-    }
-
-    private static boolean isFollowedByOtherFlag(List<String> arguments, int index) {
-        return arguments.get(index + 1).startsWith("-");
-    }
-
-    private static boolean isReachEndOfList(List<String> arguments, int index) {
-        return index + 1 >= arguments.size();
+    private static List<String> values(List<String> arguments, int index) {
+        int followingFlag = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst().orElse(arguments.size());
+        return arguments.subList(index + 1, followingFlag);
     }
 }
