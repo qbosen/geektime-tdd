3e96908ee2b07bf5753d1e8cd527b2b2bc2367ae
qiubaisen <abosen@qq.com>
2022-08-29 17:50:30 +0800

    refactor(01): 参数解析

--- args/src/main/java/top/abosen/geektime/tdd/args/Args.java
+++ args/src/main/java/top/abosen/geektime/tdd/args/Args.java
@@ -5,17 +5,30 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * @author qiubaisen
  * @date 2022/8/29
  */
 public class Args {
+
+    public static final String FLAG_PATTERN = "^-[a-zA-Z-]+$";
+
     public static <T> T parse(Class<T> optionClass, String... args) {
         Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
         Parameter[] parameters = constructor.getParameters();
         List<String> arguments = Arrays.asList(args);
-        Parameter parameter = parameters[0];
+        Object[] value = Arrays.stream(parameters).map(p -> parseOption(p, arguments)).toArray();
+
+        try {
+            return (T) constructor.newInstance(value);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Object parseOption(Parameter parameter, List<String> arguments) {
         Object value = null;
         Option option = parameter.getAnnotation(Option.class);
 
@@ -27,19 +40,20 @@ public class Args {
         } else if (parameter.getType() == String.class) {
             int index = arguments.indexOf("-" + option.value());
             value = arguments.get(index + 1);
-        }else if(parameter.getType() == String[].class){
+        } else if (parameter.getType() == String[].class) {
             int index = arguments.indexOf("-" + option.value());
-            value  = arguments.subList(index+1, arguments.size()).toArray(String[]::new);
+            int nextFlagIndex = IntStream.range(index + 1, arguments.size())
+                    .filter(it -> arguments.get(it).matches(FLAG_PATTERN))
+                    .findFirst().orElseGet(arguments::size);
+            value = arguments.subList(index + 1, nextFlagIndex).toArray(String[]::new);
         } else if (parameter.getType() == int[].class) {
             int index = arguments.indexOf("-" + option.value());
-            value  = arguments.subList(index+1, arguments.size()).stream().mapToInt(Integer::parseInt).toArray();
+            int nextFlagIndex = IntStream.range(index + 1, arguments.size())
+                    .filter(it -> arguments.get(it).matches(FLAG_PATTERN))
+                    .findFirst().orElseGet(arguments::size);
+            value = arguments.subList(index + 1, nextFlagIndex).stream().mapToInt(Integer::parseInt).toArray();
         }
-
-        try {
-            return (T) constructor.newInstance(value);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+        return value;
     }
 
 }
