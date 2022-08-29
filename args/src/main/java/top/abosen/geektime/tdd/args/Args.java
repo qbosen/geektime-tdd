7d9e754a1b0d00480b54b7c81b8f351668adb5fd
qiubaisen <abosen@qq.com>
2022-08-29 23:11:12 +0800

    refactor(03): 切换构造函数到工厂方法
    1. 显示创建构造函数
    2. 重构: 使用工厂方法替换构造函数
    3. 重构: 工厂函数返回值尽量使用接口
    4. 重构: 将工厂方法移动出去
    5. 删除原类型

--- args/src/main/java/top/abosen/geektime/tdd/args/Args.java
+++ args/src/main/java/top/abosen/geektime/tdd/args/Args.java
@@ -6,15 +6,12 @@ import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 /**
  * @author qiubaisen
  * @date 2022/8/29
  */
 public class Args {
-
-    public static final String FLAG_PATTERN = "^-[a-zA-Z-]+$";
 
     public static <T> T parse(Class<T> optionClass, String... args) {
         Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
@@ -36,74 +33,16 @@ public class Args {
     }
 
     private static final Map<Class<?>, OptionParser<?>> PARSER_MAP = Map.of(
-            int.class, new IntParser(),
-            String.class, new StringParser(),
-            boolean.class, new BooleanParser(),
-            int[].class, new IntArrayParser(),
-            String[].class, new StringArrayParser()
+            int.class, OptionParsers.createIntParser(),
+            String.class, OptionParsers.createStringParser(),
+            boolean.class, OptionParsers.BooleanParser.createBooleanParser(),
+            int[].class, OptionParsers.createIntArrayParser(),
+            String[].class, OptionParsers.createStringArrayParser()
     );
 
     private static <T> OptionParser<T> getOptionParser(Class<T> type) {
         return (OptionParser<T>) PARSER_MAP.get(type);
     }
 
-    static class BooleanParser implements OptionParser<Boolean> {
-        @Override
-        public Boolean parse(Option option, List<String> arguments) {
-            Boolean value;
-            value = arguments.contains("-" + option.value());
-            return value;
-        }
-    }
 
-    static class StringParser implements OptionParser<String> {
-        @Override
-        public String parse(Option option, List<String> arguments) {
-            String value;
-            int index = arguments.indexOf("-" + option.value());
-            value = arguments.get(index + 1);
-            return value;
-        }
-    }
-
-    static class StringArrayParser implements OptionParser<String[]> {
-        @Override
-        public String[] parse(Option option, List<String> arguments) {
-            String[] value;
-            int index = arguments.indexOf("-" + option.value());
-            int nextFlagIndex = IntStream.range(index + 1, arguments.size())
-                    .filter(it -> arguments.get(it).matches(FLAG_PATTERN))
-                    .findFirst().orElseGet(arguments::size);
-            value = arguments.subList(index + 1, nextFlagIndex).toArray(String[]::new);
-            return value;
-        }
-    }
-
-    static class IntParser implements OptionParser<Integer> {
-        @Override
-        public Integer parse(Option option, List<String> arguments) {
-            Integer value;
-            int index = arguments.indexOf("-" + option.value());
-            value = Integer.parseInt(arguments.get(index + 1));
-            return value;
-        }
-    }
-
-    static class IntArrayParser implements OptionParser<int[]> {
-        @Override
-        public int[] parse(Option option, List<String> arguments) {
-            int[] value;
-            int index = arguments.indexOf("-" + option.value());
-            int nextFlagIndex = IntStream.range(index + 1, arguments.size())
-                    .filter(it -> arguments.get(it).matches(FLAG_PATTERN))
-                    .findFirst().orElseGet(arguments::size);
-            value = arguments.subList(index + 1, nextFlagIndex).stream().mapToInt(Integer::parseInt).toArray();
-            return value;
-        }
-    }
-
-
-    interface OptionParser<T> {
-        T parse(Option option, List<String> arguments);
-    }
 }
---
+++ args/src/main/java/top/abosen/geektime/tdd/args/OptionParsers.java
@@ -0,0 +1,82 @@
+package top.abosen.geektime.tdd.args;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+
+/**
+ * @author qiubaisen
+ * @date 2022/8/29
+ */
+public class OptionParsers {
+    private OptionParsers() {
+    }
+
+    private static final String FLAG_PATTERN = "^-[a-zA-Z-]+$";
+
+    public static OptionParser<Integer[]> createIntArrayParser() {
+        return new ArrayValueParser<>(Integer::parseInt, Integer[]::new);
+    }
+
+    public static OptionParser<Integer> createIntParser() {
+        return new SingleValueParser<>(Integer::parseInt);
+    }
+
+    public static OptionParser<String[]> createStringArrayParser() {
+        return new ArrayValueParser<>(String::valueOf, String[]::new);
+    }
+
+    public static OptionParser<String> createStringParser() {
+        return new SingleValueParser<>(String::valueOf);
+    }
+
+    static class ArrayValueParser<T> implements OptionParser<T[]> {
+        private final Function<String, T> parser;
+        private final IntFunction<T[]> generator;
+
+        public ArrayValueParser(Function<String, T> parser, IntFunction<T[]> generator) {
+            this.parser = parser;
+            this.generator = generator;
+        }
+
+        @Override
+        public T[] parse(Option option, List<String> arguments) {
+            int index = arguments.indexOf("-" + option.value());
+            int nextFlagIndex = IntStream.range(index + 1, arguments.size())
+                    .filter(it -> arguments.get(it).matches(FLAG_PATTERN))
+                    .findFirst().orElseGet(arguments::size);
+            return arguments.subList(index + 1, nextFlagIndex).stream().map(parser).toArray(generator);
+        }
+    }
+
+    static class SingleValueParser<T> implements OptionParser<T> {
+        private final Function<String, T> parser;
+
+        public SingleValueParser(Function<String, T> parser) {
+            this.parser = parser;
+        }
+
+        @Override
+        public T parse(Option option, List<String> arguments) {
+            int index = arguments.indexOf("-" + option.value());
+            return parser.apply(arguments.get(index + 1));
+        }
+    }
+
+    static class BooleanParser implements OptionParser<Boolean> {
+        private BooleanParser() {
+        }
+
+        public static OptionParser<Boolean> createBooleanParser() {
+            return new BooleanParser();
+        }
+
+        @Override
+        public Boolean parse(Option option, List<String> arguments) {
+            Boolean value;
+            value = arguments.contains("-" + option.value());
+            return value;
+        }
+    }
+}
