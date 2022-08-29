cb4408c21c05bc25e50fbb67445d23aa6df856d7
qiubaisen <abosen@qq.com>
2022-08-29 17:33:40 +0800

    feat: 红绿 完成基本功能
    接下来重构消除坏味道

--- args/src/main/java/top/abosen/geektime/tdd/args/Args.java
+++ args/src/main/java/top/abosen/geektime/tdd/args/Args.java
@@ -1,11 +1,45 @@
 package top.abosen.geektime.tdd.args;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author qiubaisen
  * @date 2022/8/29
  */
 public class Args {
     public static <T> T parse(Class<T> optionClass, String... args) {
-        return null;
+        Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
+        Parameter[] parameters = constructor.getParameters();
+        List<String> arguments = Arrays.asList(args);
+        Parameter parameter = parameters[0];
+        Object value = null;
+        Option option = parameter.getAnnotation(Option.class);
+
+        if (parameter.getType() == boolean.class) {
+            value = arguments.contains("-" + option.value());
+        } else if (parameter.getType() == int.class) {
+            int index = arguments.indexOf("-" + option.value());
+            value = Integer.parseInt(arguments.get(index + 1));
+        } else if (parameter.getType() == String.class) {
+            int index = arguments.indexOf("-" + option.value());
+            value = arguments.get(index + 1);
+        }else if(parameter.getType() == String[].class){
+            int index = arguments.indexOf("-" + option.value());
+            value  = arguments.subList(index+1, arguments.size()).toArray(String[]::new);
+        } else if (parameter.getType() == int[].class) {
+            int index = arguments.indexOf("-" + option.value());
+            value  = arguments.subList(index+1, arguments.size()).stream().mapToInt(Integer::parseInt).toArray();
+        }
+
+        try {
+            return (T) constructor.newInstance(value);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
--- args/src/test/java/top/abosen/geektime/tdd/args/ArgsTest.java
+++ args/src/test/java/top/abosen/geektime/tdd/args/ArgsTest.java
@@ -11,22 +11,61 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class ArgsTest {
     // happy path
-    // todo boolean -l
-    // todo integer -p 8080
-    // todo string -d usr/logs
+    // boolean -l
+    @Test
+    public void should_parse_bool() {
+        record Options(@Option("l") boolean logging) {
+        }
+        Options options = Args.parse(Options.class, "-l");
+        assertTrue(options.logging());
+    }
+
+    //  integer -p 8080
+    @Test
+    public void should_parse_integer() {
+        record Options(@Option("p") int port) {
+        }
+        Options options = Args.parse(Options.class, "-p", "8080");
+        assertEquals(8080, options.port());
+    }
+
+    //  string -d usr/logs
+    @Test
+    public void should_parse_string() {
+        record Options(@Option("d") String directory) {
+        }
+        Options options = Args.parse(Options.class, "-d", "usr/logs");
+        assertEquals("usr/logs", options.directory());
+    }
+
     // todo array: -g this is/ -d 1 2 -3
+    @Test
+    public void should_parse_string_array() {
+        record Options(@Option("g") String[] groups) {
+        }
+        Options options = Args.parse(Options.class, "-g", "this", "is");
+        assertArrayEquals(new String[]{"this", "is"}, options.groups());
+    }
+
+    @Test
+    public void should_parse_integer_array() {
+        record Options(@Option("d") int[] decimals) {
+        }
+        Options options = Args.parse(Options.class, "-d", "1", "2", "-3");
+        assertArrayEquals(new int[]{1, 2, -3}, options.decimals());
+    }
 
 
-    // todo -l -p 8080 -d /usr/logs
+    // todo example1: -l -p 8080 -d /usr/logs
     @Test
     @Disabled
     public void should_example_1() {
         record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
         }
         Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging);
-        assertEquals(8080, options.port);
-        assertEquals("/usr/logs", options.directory);
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
     }
 
 
@@ -37,8 +76,8 @@ public class ArgsTest {
         record Options(@Option("g") String[] groups, @Option("d") int[] decimals) {
         }
         Options options = Args.parse(Options.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
-        assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.groups);
-        assertEquals(new int[]{1, 2, -3, 5}, options.decimals);
+        assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.groups());
+        assertEquals(new int[]{1, 2, -3, 5}, options.decimals());
     }
 
     // sad path
