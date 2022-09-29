3ad0b6a080cac399c022bf8bc0cb89a1821e7096
qiubaisen <abosen@qq.com>
2022-09-29 15:47:30 +0800

    feat: 简易的demo

--- 02-args-other/src/main/java/top/abosen/geektime/tdd/args/Args.java
+++ 02-args-other/src/main/java/top/abosen/geektime/tdd/args/Args.java
@@ -1,15 +1,47 @@
 package top.abosen.geektime.tdd.args;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.*;
 
 /**
  * @author qiubaisen
  * @date 2022/9/29
  */
-public class Args {
+public class Args<T> {
+
+    private final Class<T> optionClass;
+    private final Map<Class<?>, OptionParser<?>> parsers;
+
+    protected Args(Class<T> optionClass, Map<Class<?>, OptionParser<?>> parsers) {
+        this.parsers = parsers;
+        this.optionClass = optionClass;
+    }
+
+
+    public T parse(String... args) {
+        Map<String, String[]> options = toMap(args);
+        Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
+        Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(options, it)).toArray();
+        try {
+            return (T) constructor.newInstance(values);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object parseOption(Map<String, String[]> options, Parameter parameter) {
+        if (!parameter.isAnnotationPresent(Option.class)) {
+            throw new RuntimeException();
+        }
+        if (!parsers.containsKey(parameter.getType())) {
+            throw new RuntimeException();
+        }
+        Option option = parameter.getAnnotation(Option.class);
+        return parsers.get(parameter.getType()).parse(options.get(option.value()));
+    }
+
     public static Map<String, String[]> toMap(String... args) {
         Map<String, String[]> result = new HashMap<>();
 
--- 02-args-other/src/test/java/top/abosen/geektime/tdd/args/ArgsTest.java
+++ 02-args-other/src/test/java/top/abosen/geektime/tdd/args/ArgsTest.java
@@ -1,62 +1,43 @@
 package top.abosen.geektime.tdd.args;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author qiubaisen
  * @date 2022/9/29
  */
 class ArgsTest {
-    // option without value -b
-    @Test
-    public void should_split_option_without_value() {
-        Map<String, String[]> args = Args.toMap("-b");
 
-        assertEquals(1, args.size());
-        assertArrayEquals(new String[]{}, args.get("b"));
-    }
-
-    // option with value -p 8080
-    @Test
-    public void should_split_option_with_value() {
-        Map<String, String[]> args = Args.toMap("-p", "8080");
-
-        assertEquals(1, args.size());
-        assertArrayEquals(new String[]{"8080"}, args.get("p"));
-    }
-
-    // option with values -g this is a list
-    @Test
-    public void should_split_option_with_values() {
-        Map<String, String[]> args = Args.toMap("-g", "this", "is", "a", "list");
-
-        assertEquals(1, args.size());
-        assertArrayEquals(new String[]{"this", "is", "a", "list"}, args.get("g"));
-    }
-
-    // multiple options
-    @Test
-    public void should_split_args_to_map() {
-        Map<String, String[]> args = Args.toMap("-b", "-p", "8080", "-d", "/usr/logs");
-
-        assertEquals(3, args.size());
-        assertArrayEquals(new String[]{}, args.get("b"));
-        assertArrayEquals(new String[]{"8080"}, args.get("p"));
-        assertArrayEquals(new String[]{"/usr/logs"}, args.get("d"));
+    private static String[] checkSize(String[] values, int size) {
+        if (values != null && values.length != size) {
+            throw new RuntimeException();
+        }
+        return values;
     }
 
     @Test
-    public void should_split_args_list_to_map() {
-        Map<String, String[]> args = Args.toMap("-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
-
-        assertEquals(2, args.size());
-        assertArrayEquals(new String[]{"this", "is", "a", "list"}, args.get("g"));
-        assertArrayEquals(new String[]{"1", "2", "-3", "5"}, args.get("d"));
+    public void should_parse_bool_option() {
+        Args<BoolOption> args = new Args<>(BoolOption.class, Map.of(boolean.class, values -> checkSize(values, 0) != null));
+        BoolOption option = args.parse("-l");
+        assertTrue(option.logging);
     }
+
+    @Test
+    public void should_parse_int_option() {
+        Args<IntOption> args = new Args<>(IntOption.class, Map.of(int.class, values -> Integer.parseInt(checkSize(values, 1)[0])));
+        IntOption option = args.parse("-p", "8080");
+        assertEquals(8080, option.port());
+    }
+
+    record BoolOption(@Option("l") boolean logging) {
+    }
+
+    record IntOption(@Option("p") int port) {
+    }
+
 }
