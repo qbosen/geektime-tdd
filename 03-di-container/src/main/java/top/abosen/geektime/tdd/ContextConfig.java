322d00de7de869154807952bcf317bc77387a494
qiubaisen <abosen@qq.com>
2022-10-19 17:53:34 +0800

    refactor(di): 内聚构造器注入实现,分离单元

--- 03-di-container/src/main/java/top/abosen/geektime/tdd/ContextConfig.java
+++ 03-di-container/src/main/java/top/abosen/geektime/tdd/ContextConfig.java
@@ -1,12 +1,6 @@
 package top.abosen.geektime.tdd;
 
-import jakarta.inject.Inject;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Parameter;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author qiubaisen
@@ -38,9 +32,8 @@ public class ContextConfig {
 
     public <Type, Implementation extends Type>
     void bind(Class<Type> type, Class<Implementation> implementation) {
-        Constructor<Implementation> injectConstructor = getInjectConstructor(implementation);
 
-        providers.put(type, new ConstructorInjectionProvider(injectConstructor));
+        providers.put(type, new ConstructorInjectionProvider<>(implementation));
     }
 
     public Context getContext() {
@@ -74,45 +67,4 @@ public class ContextConfig {
         }
     }
 
-    private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
-        List<Constructor<?>> injectConstructors = Arrays.stream(implementation.getConstructors())
-                .filter(it -> it.isAnnotationPresent(Inject.class)).toList();
-
-        if (injectConstructors.size() > 1) {
-            throw new IllegalComponentException();
-        }
-
-        return (Constructor<Type>) injectConstructors.stream().findFirst().orElseGet(() -> {
-            try {
-                return implementation.getConstructor();
-            } catch (NoSuchMethodException e) {
-                throw new IllegalComponentException();
-            }
-        });
-    }
-
-    private static final class ConstructorInjectionProvider<Type> implements ComponentProvider<Type> {
-        private final Constructor<Type> injectConstructor;
-
-        public ConstructorInjectionProvider(Constructor<Type> injectConstructor) {
-            this.injectConstructor = injectConstructor;
-        }
-
-        @Override
-        public List<Class<?>> getDependencies() {
-            return Arrays.stream(injectConstructor.getParameters()).map(Parameter::getType).collect(Collectors.toList());
-        }
-
-        @Override
-        public Type get(Context context) {
-            try {
-                Object[] dependencies = Arrays.stream(injectConstructor.getParameters())
-                        .map(parameter -> context.get(parameter.getType())).toArray();
-                return injectConstructor.newInstance(dependencies);
-            } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-    }
 }
