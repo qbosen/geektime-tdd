9826de187eefdf76bf567493cb21c4a33aa934d3
qiubaisen <abosen@qq.com>
2022-11-30 18:12:09 +0800

    test(spike): 通过Providers实现MessageBodyWriter

--- 04-restful/src/test/java/top/abosen/geektim/tdd/rest/ASpike.java
+++ 04-restful/src/test/java/top/abosen/geektim/tdd/rest/ASpike.java
@@ -6,7 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -17,13 +21,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -41,7 +50,8 @@ public class ASpike {
         server.addConnector(connector);
 
         ServletContextHandler handler = new ServletContextHandler(server, "/");
-        handler.addServlet(new ServletHolder(new ResourceServlet(new TestApplication())), "/");
+        TestApplication application = new TestApplication();
+        handler.addServlet(new ServletHolder(new ResourceServlet(application, new TestProviders(application))), "/");
 
         server.setHandler(handler);
         server.start();
@@ -65,10 +75,13 @@ public class ASpike {
 
     static class ResourceServlet extends HttpServlet {
         private Application application;
+        private Providers providers;
 
-        public ResourceServlet(Application application) {
+        public ResourceServlet(Application application, Providers providers) {
             this.application = application;
+            this.providers = providers;
         }
+
 
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -76,16 +89,14 @@ public class ASpike {
 
             Object result = dispatch(req, rootResources);
 
-            resp.getWriter().write(result.toString());
-            resp.getWriter().flush();
+            MessageBodyWriter<Object> writer = (MessageBodyWriter<Object>) providers.getMessageBodyWriter(result.getClass(), null, null, null);
+            writer.writeTo(result, null, null, null, null, null, resp.getOutputStream());
         }
 
         Object dispatch(HttpServletRequest req, Stream<Class<?>> rootResources) {
             try {
                 Class<?> rootClass = rootResources.findFirst().get();
-                Constructor<?> constructor = rootClass.getDeclaredConstructor();
-                constructor.setAccessible(true);
-                Object rootResource = constructor.newInstance();
+                Object rootResource = instance(rootClass);
                 Method method = Arrays.stream(rootClass.getMethods()).filter(m -> m.isAnnotationPresent(GET.class)).findFirst().get();
 
                 return method.invoke(rootResource);
@@ -95,10 +106,67 @@ public class ASpike {
         }
     }
 
+    private static Object instance(Class<?> c) {
+        try {
+            Constructor<?> constructor = c.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static class TestProviders implements Providers {
+        private final List<MessageBodyWriter> writers;
+
+        private Application application;
+
+        public TestProviders(Application application) {
+            this.application = application;
+            writers = this.application.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom)
+                    .map(c -> (MessageBodyWriter) instance(c)).toList();
+        }
+
+        @Override
+        public <T> MessageBodyReader<T> getMessageBodyReader(Class<T> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return null;
+        }
+
+        @Override
+        public <T> MessageBodyWriter<T> getMessageBodyWriter(Class<T> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return (MessageBodyWriter<T>) writers.stream().filter(w -> w.isWriteable(type, genericType, annotations, mediaType))
+                    .findFirst().get();
+        }
+
+        @Override
+        public <T extends Throwable> ExceptionMapper<T> getExceptionMapper(Class<T> type) {
+            return null;
+        }
+
+        @Override
+        public <T> ContextResolver<T> getContextResolver(Class<T> contextType, MediaType mediaType) {
+            return null;
+        }
+    }
+
+    static class StringMessageBodyWriter implements MessageBodyWriter<String> {
+        @Override
+        public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return type == String.class;
+        }
+
+        @Override
+        public void writeTo(String s, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+            PrintWriter writer = new PrintWriter(entityStream);
+            writer.write(s);
+            writer.flush();
+        }
+    }
+
     static class TestApplication extends Application {
         @Override
         public Set<Class<?>> getClasses() {
-            return Set.of(TestResource.class);
+            return Set.of(TestResource.class, StringMessageBodyWriter.class);
         }
     }
 
