9a9686cc2d12f3719432c98d7c5b8f2b20c67b14
qiubaisen <abosen@qq.com>
2022-11-30 23:26:25 +0800

    test(rest): 使用di容器管理实例

--- 04-restful/src/test/java/top/abosen/geektim/tdd/rest/ASpike.java
+++ 04-restful/src/test/java/top/abosen/geektim/tdd/rest/ASpike.java
@@ -1,5 +1,7 @@
 package top.abosen.geektim.tdd.rest;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,12 +21,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import top.abosen.geektime.tdd.di.ComponentRef;
+import top.abosen.geektime.tdd.di.Config;
+import top.abosen.geektime.tdd.di.Context;
+import top.abosen.geektime.tdd.di.ContextConfig;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.net.URI;
@@ -70,16 +75,18 @@ public class ASpike {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
-        Assertions.assertEquals("test", response.body());
+        Assertions.assertEquals("prefixprefixtest", response.body());
     }
 
     static class ResourceServlet extends HttpServlet {
-        private Application application;
+        private final Context context;
+        private TestApplication application;
         private Providers providers;
 
-        public ResourceServlet(Application application, Providers providers) {
+        public ResourceServlet(TestApplication application, Providers providers) {
             this.application = application;
             this.providers = providers;
+            context = application.getContext();
         }
 
 
@@ -97,7 +104,7 @@ public class ASpike {
         Object dispatch(HttpServletRequest req, Stream<Class<?>> rootResources) {
             try {
                 Class<?> rootClass = rootResources.findFirst().get();
-                Object rootResource = instance(rootClass);
+                Object rootResource = context.get(ComponentRef.of(rootClass));
                 Method method = Arrays.stream(rootClass.getMethods()).filter(m -> m.isAnnotationPresent(GET.class)).findFirst().get();
 
                 return method.invoke(rootResource);
@@ -107,26 +114,17 @@ public class ASpike {
         }
     }
 
-    private static Object instance(Class<?> c) {
-        try {
-            Constructor<?> constructor = c.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            return constructor.newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     //application scope
     static class TestProviders implements Providers {
         private final List<MessageBodyWriter> writers;
 
-        private Application application;
+        private TestApplication application;
 
-        public TestProviders(Application application) {
+        public TestProviders(TestApplication application) {
             this.application = application;
-            writers = this.application.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom)
-                    .map(c -> (MessageBodyWriter) instance(c)).toList();
+            List<Class<?>> writerClasses = this.application.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom).toList();
+            Context context = application.getContext();
+            writers = (List<MessageBodyWriter>) writerClasses.stream().map(c -> context.get(ComponentRef.of(c))).toList();
         }
 
         @Override
@@ -152,6 +150,10 @@ public class ASpike {
     }
 
     static class StringMessageBodyWriter implements MessageBodyWriter<String> {
+        @Inject
+        @Named("prefix")
+        String prefix;
+
         @Override
         public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
             return type == String.class;
@@ -160,23 +162,57 @@ public class ASpike {
         @Override
         public void writeTo(String s, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
             PrintWriter writer = new PrintWriter(entityStream);
+            writer.write(prefix);
             writer.write(s);
             writer.flush();
         }
     }
 
     static class TestApplication extends Application {
+        Context context;
         @Override
         public Set<Class<?>> getClasses() {
             return Set.of(TestResource.class, StringMessageBodyWriter.class);
+        }
+
+        public Config getConfig() {
+            return new Config() {
+                @Named("prefix")
+                public String name = "prefix";
+            };
+        }
+
+        public Context getContext(){
+            return context;
+        }
+
+        public TestApplication() {
+            ContextConfig config = new ContextConfig();
+            config.from(getConfig());
+
+            List<Class<?>> writerClasses = this.getClasses().stream().filter(MessageBodyWriter.class::isAssignableFrom).toList();
+            for (Class writerClass : writerClasses) {
+                config.bindComponent(writerClass, writerClass);
+            }
+
+            List<Class<?>> rootResources = this.getClasses().stream().filter(c -> c.isAnnotationPresent(Path.class)).toList();
+            for (Class rootResource : rootResources) {
+                config.bindComponent(rootResource, rootResource);
+            }
+
+            context = config.getContext();
         }
     }
 
     @Path("/test")
     static class TestResource {
+        @Inject
+        @Named("prefix")
+        String prefix;
+
         @GET
         public String get() {
-            return "test";
+            return prefix + "test";
         }
     }
 
