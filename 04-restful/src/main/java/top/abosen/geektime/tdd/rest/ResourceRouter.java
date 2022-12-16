26883953fe2e75e6470ffad6d69ae4db765c4a85
qiubaisen <abosen@qq.com>
2022-12-16 17:04:25 +0800

    refactor(dispatcher#52): 使用方法名清晰函数式调用过程

--- 04-restful/src/main/java/top/abosen/geektime/tdd/rest/ResourceRouter.java
+++ 04-restful/src/main/java/top/abosen/geektime/tdd/rest/ResourceRouter.java
@@ -7,7 +7,6 @@ import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,26 +43,40 @@ class DefaultResourceRouter implements ResourceRouter {
     @Override
     public OutboundResponse dispatch(HttpServletRequest request, ResourceContext resourceContext) {
         String path = request.getServletPath();
-
         UriInfoBuilder uri = runtime.createUriInfoBuilder(request);
-        Optional<Result> matched = rootResources.stream().map(it -> new Result(it.getUriTemplate().match(path), it))
-                .filter(it -> it.matched.isPresent())
-                .sorted(Comparator.comparing(it -> it.matched.get()))
-                .findFirst();
 
-        Optional<ResourceMethod> method = matched.flatMap(resource -> resource.resource.match(resource.matched.get().getRemaining(), request.getMethod(),
-                Collections.list(request.getHeaders(HttpHeaders.ACCEPT)).toArray(String[]::new),
-                uri
-        ));
-
-        return (OutboundResponse) method.map(m -> Optional.ofNullable(m.call(resourceContext, uri))
-                        .map(entity -> OutboundResponse.ok(entity).build())
+        return (OutboundResponse) rootResources.stream().map(resource -> match(path, resource))
+                .filter(Result::isMatched).sorted().findFirst()
+                .flatMap(result -> result.findResourceMethod(request, uri))
+                .map(m -> callMethod(resourceContext, uri, m)
+                        .map(entity -> Response.ok(entity).build())
                         .orElseGet(() -> Response.noContent().build()))
-                .orElseGet(() -> OutboundResponse.status(Response.Status.NOT_FOUND).build())
+                .orElseGet(() -> Response.status(Response.Status.NOT_FOUND).build())
                 ;
     }
 
-    record Result(Optional<UriTemplate.MatchResult> matched, RootResource resource) {
+    private static Optional<? extends GenericEntity<?>> callMethod(ResourceContext resourceContext, UriInfoBuilder uri, ResourceMethod m) {
+        return Optional.ofNullable(m.call(resourceContext, uri));
+    }
+
+    private static Result match(String path, RootResource it) {
+        return new Result(it.getUriTemplate().match(path), it);
+    }
+
+    record Result(Optional<UriTemplate.MatchResult> matched, RootResource resource) implements Comparable<Result> {
+        private Optional<ResourceMethod> findResourceMethod(HttpServletRequest request, UriInfoBuilder uri) {
+            return matched.flatMap(it -> resource.match(it.getRemaining(), request.getMethod(),
+                    Collections.list(request.getHeaders(HttpHeaders.ACCEPT)).toArray(String[]::new), uri));
+        }
+
+        public boolean isMatched() {
+            return matched.isPresent();
+        }
+
+        @Override
+        public int compareTo(Result o) {
+            return matched.flatMap(x -> o.matched.map(x::compareTo)).orElse(0);
+        }
     }
 
 }
