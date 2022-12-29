eea0d375bf4f958636abdd2b5ee835149f3aa447
qiubaisen <abosen@qq.com>
2022-12-29 14:48:06 +0800

    test(method-call#75): introduce common test part for InjectableCallerTest

--- 04-restful/src/test/java/top/abosen/geektime/tdd/rest/DefaultResourceMethodTest.java
+++ 04-restful/src/test/java/top/abosen/geektime/tdd/rest/DefaultResourceMethodTest.java
@@ -7,77 +7,33 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.container.ResourceContext;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.GenericEntity;
-import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.UriInfo;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
 
 import java.lang.reflect.Proxy;
 import java.math.BigDecimal;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * @author qiubaisen
  * @date 2022/12/27
  */
-public class DefaultResourceMethodTest {
+public class DefaultResourceMethodTest extends InjectableCallerTest {
 
-    private CallableResourceMethods resource;
-    private ResourceContext context;
-    private UriInfoBuilder builder;
-    private UriInfo uriInfo;
-    private MultivaluedHashMap<String, String> parameters;
-
-    private LastCall lastCall;
-    private SomeServiceInContext service;
-
-    record LastCall(String name, List<Object> arguments) {
-
-    }
-
-    @BeforeEach
-    public void setup() {
-        lastCall = null;
-        resource = (CallableResourceMethods) Proxy.newProxyInstance(this.getClass().getClassLoader(),
+    @Override
+    protected Object initResource() {
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(),
                 new Class[]{CallableResourceMethods.class},
                 (proxy, method, args) -> {
                     String name = method.getName();
-
                     lastCall = new LastCall(
                             getMethodName(name, method.getParameterTypes()),
                             args == null ? List.of() : List.of(args));
                     return "getList".equals(name) ? List.of() : null;
                 });
-
-        context = mock(ResourceContext.class);
-        builder = mock(UriInfoBuilder.class);
-        uriInfo = mock(UriInfo.class);
-        service = mock(SomeServiceInContext.class);
-        parameters = new MultivaluedHashMap<>();
-
-        when(builder.getLastMatchedResource()).thenReturn(resource);
-        when(builder.createUriInfo()).thenReturn(uriInfo);
-        when(uriInfo.getPathParameters()).thenReturn(parameters);
-        when(uriInfo.getQueryParameters()).thenReturn(parameters);
-        when(context.getResource(eq(SomeServiceInContext.class))).thenReturn(service);
-    }
-
-    private static String getMethodName(String name, Class<?>... types) {
-        return name + Arrays.stream(types)
-                .map(Class::getSimpleName)
-                .collect(Collectors.joining(",", "(", ")"));
     }
 
     @Test
@@ -100,64 +56,10 @@ public class DefaultResourceMethodTest {
     }
 
 
-    record InjectableTypeTestCase(Class<?> type, String string, Object value) {
-    }
-
-
-    @TestFactory
-    public List<DynamicTest> inject_convertable_types() {
-        List<DynamicTest> tests = new ArrayList<>();
-        List<InjectableTypeTestCase> typeCases = List.of(
-                new InjectableTypeTestCase(int.class, "1", 1),
-                new InjectableTypeTestCase(short.class, "3", (short) 3),
-                new InjectableTypeTestCase(float.class, "8.33", 8.33f),
-                new InjectableTypeTestCase(double.class, "3.25", 3.25d),
-                new InjectableTypeTestCase(byte.class, "111", (byte) 111),
-                new InjectableTypeTestCase(boolean.class, "true", true),
-                new InjectableTypeTestCase(BigDecimal.class, "12345", new BigDecimal("12345")),
-                new InjectableTypeTestCase(Converter.class, "Factory", Converter.Factory),
-                new InjectableTypeTestCase(String.class, "string", "string")
-        );
-        List<String> paramTypes = List.of("getPathParam", "getQueryParam");
-
-        for (String type : paramTypes) {
-            for (InjectableTypeTestCase typeCase : typeCases) {
-                tests.add(DynamicTest.dynamicTest(
-                        MessageFormat.format("should inject {0} to {1}", typeCase.type.getSimpleName(), type),
-                        () -> verifyResourceMethodCalled(type, typeCase.type, typeCase.string, typeCase.value)
-                ));
-            }
-        }
-        return tests;
-    }
-
-    @TestFactory
-    public List<DynamicTest> inject_context_object() {
-        List<DynamicTest> tests = new ArrayList<>();
-        List<InjectableTypeTestCase> typeCases = List.of(
-                new InjectableTypeTestCase(SomeServiceInContext.class, "N/A", service),
-                new InjectableTypeTestCase(ResourceContext.class, "N/A", context),
-                new InjectableTypeTestCase(UriInfo.class, "N/A", uriInfo)
-        );
-        List<String> paramTypes = List.of("getContext");
-
-        for (String type : paramTypes) {
-            for (InjectableTypeTestCase typeCase : typeCases) {
-                tests.add(DynamicTest.dynamicTest(
-                        MessageFormat.format("should inject {0} to getContext {1}", typeCase.type.getSimpleName(), type),
-                        () -> verifyResourceMethodCalled(type, typeCase.type, typeCase.string, typeCase.value)
-                ));
-            }
-        }
-        return tests;
-    }
-
-    private void verifyResourceMethodCalled(String method, Class<?> type, String parameterValue, Object value) throws NoSuchMethodException {
+    @Override
+    protected void callInjectable(String method, Class<?> type) throws NoSuchMethodException {
         DefaultResourceMethod resourceMethod = getResourceMethod(method, type);
-        parameters.put("param", List.of(parameterValue));
         resourceMethod.call(context, builder);
-        assertEquals(getMethodName(method, type), lastCall.name);
-        assertEquals(List.of(value), lastCall.arguments);
     }
 
 
--- 04-restful/src/test/java/top/abosen/geektime/tdd/rest/SubResourceLocatorTest.java
+++ 04-restful/src/test/java/top/abosen/geektime/tdd/rest/SubResourceLocatorTest.java
@@ -3,45 +3,30 @@ package top.abosen.geektime.tdd.rest;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.container.ResourceContext;
-import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.UriInfo;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * @author qiubaisen
  * @date 2022/12/29
  */
-class SubResourceLocatorTest {
-    private SubResourceMethods resource;
-    private ResourceContext context;
-    private UriInfoBuilder builder;
-    private UriInfo uriInfo;
-    private MultivaluedHashMap<String, String> parameters;
+class SubResourceLocatorTest extends InjectableCallerTest{
 
-    private LastCall lastCall;
-    private UriTemplate.MatchResult matchResult;
 
-    record LastCall(String name, List<Object> arguments) {
-
-    }
-
-    @BeforeEach
-    public void before() {
-        lastCall = null;
-        resource = (SubResourceMethods) Proxy.newProxyInstance(this.getClass().getClassLoader(),
+    @Override
+    protected Object initResource() {
+        return  Proxy.newProxyInstance(this.getClass().getClassLoader(),
                 new Class[]{SubResourceMethods.class},
                 (proxy, method, args) -> {
                     String name = method.getName();
@@ -51,39 +36,92 @@ class SubResourceLocatorTest {
                             args == null ? List.of() : List.of(args));
                     return new Message();
                 });
-
-        context = mock(ResourceContext.class);
-        builder = mock(UriInfoBuilder.class);
-        uriInfo = mock(UriInfo.class);
-        matchResult = mock(UriTemplate.MatchResult.class);
-        parameters = new MultivaluedHashMap<>();
-
-        when(builder.getLastMatchedResource()).thenReturn(resource);
-        when(builder.createUriInfo()).thenReturn(uriInfo);
-        when(uriInfo.getPathParameters()).thenReturn(parameters);
-        when(uriInfo.getQueryParameters()).thenReturn(parameters);
     }
-    private static String getMethodName(String name, Class<?>... types) {
-        return name + Arrays.stream(types)
-                .map(Class::getSimpleName)
-                .collect(Collectors.joining(",", "(", ")"));
-    }
+
+
 
     @Test
     void should_inject_string_path_param_to_sub_resource_method() throws NoSuchMethodException {
-        Method method = SubResourceMethods.class.getMethod("getPathParam", String.class);
-        SubResourceLocators.SubResourceLocator locator = new SubResourceLocators.SubResourceLocator(method);
         parameters.put("param", List.of("path"));
-        locator.match(matchResult, "GET", new String[0], context, builder).get();
 
-        assertEquals("getPathParam(String)", lastCall.name);
-        assertEquals(List.of("path"), lastCall.arguments);
+        String method = "getPathParam";
+        Class<String> type = String.class;
+        callInjectable(method, type);
+
+        assertEquals("getPathParam(String)", lastCall.name());
+        assertEquals(List.of("path"), lastCall.arguments());
+    }
+
+    @Override
+    protected void callInjectable(String method, Class<?> type) throws NoSuchMethodException {
+        SubResourceLocators.SubResourceLocator locator = new SubResourceLocators.SubResourceLocator(SubResourceMethods.class.getMethod(method, type));
+        locator.match(mock(UriTemplate.MatchResult.class), "GET", new String[0], context, builder).get();
     }
 
     @SuppressWarnings("UnresolvedRestParam")
     interface SubResourceMethods {
         @Path("/message")
-        Message getPathParam(@PathParam("param") String path);
+        Message getPathParam(@PathParam("param") int value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") short value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") float value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") double value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") byte value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") boolean value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") BigDecimal value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") Converter value);
+
+        @Path("/message")
+        Message getPathParam(@PathParam("param") String value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") int value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") short value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") float value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") double value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") byte value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") boolean value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") BigDecimal value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") Converter value);
+
+        @Path("/message")
+        Message getQueryParam(@QueryParam("param") String value);
+
+        @Path("/message")
+        Message getContext(@Context SomeServiceInContext service);
+
+        @Path("/message")
+        Message getContext(@Context ResourceContext context);
+
+        @Path("/message")
+        Message getContext(@Context UriInfo uriInfo);
     }
 
     static class Message {
