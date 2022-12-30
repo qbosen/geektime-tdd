c115681cb7101582149730b79013d9efefcec2ac
qiubaisen <abosen@qq.com>
2022-12-30 16:27:50 +0800

    refactor: get or create line

--- 05-line-editor/src/line-editor.ts
+++ 05-line-editor/src/line-editor.ts
@@ -4,39 +4,35 @@ export class LineEditor extends Konva.Group {
     private line?: Konva.Line
 
     attach(line: Konva.Line) {
-        let points = line.points();
-        let previous = -1;
-        for (let i = 0; i < points.length / 2; i++) {
-            this.add(new Konva.Circle({name: `${i}-anchor`, radius: 10, x: points[i * 2], y: points[i * 2 + 1]}));
-            if (previous != -1) {
-                this.add(new Konva.Circle({
-                    name: `${i}-control`,
-                    radius: 10,
-                    x: points[previous * 2] + (points[i * 2] - points[previous * 2]) / 2,
-                    y: points[previous * 2 + 1] + (points[i * 2 + 1] - points[previous * 2 + 1]) / 2
-                }));
-            }
-            previous = i;
-        }
-
         this.line = line;
         line.on('pointsChange', () => {
             this.update();
         });
+        this.update();
     }
 
     private update() {
         let points = this.line!.points();
         let previous = -1;
         for (let i = 0; i < points.length / 2; i++) {
-            this.findOne(`.${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
+            this.get(`${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
             if (previous != -1) {
-                this.findOne(`.${i}-control`).setAttrs({
+                this.get(`${i}-control`).setAttrs({
                     x: points[previous * 2] + (points[i * 2] - points[previous * 2]) / 2,
                     y: points[previous * 2 + 1] + (points[i * 2 + 1] - points[previous * 2 + 1]) / 2
                 });
             }
             previous = i;
         }
+    }
+
+    private get(name: string) {
+        return this.findOne(`.${name}`) || this.create(name);
+    }
+
+    private create(name: string) {
+        let point = new Konva.Circle({name: name, radius: 10});
+        this.add(point);
+        return point;
     }
 }
