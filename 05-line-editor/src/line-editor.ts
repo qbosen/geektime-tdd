75a9083b059d89dc78a99a079cf88069f7a3cf09
qiubaisen <abosen@qq.com>
2022-12-30 15:18:52 +0800

    feat: init project

---
+++ 05-line-editor/src/line-editor.ts
@@ -0,0 +1,4 @@
+import Konva from "konva";
+
+export class LineEditor extends Konva.Group {
+}
---
+++ 05-line-editor/test/line-editor.test.ts
@@ -0,0 +1,11 @@
+import {describe} from "vitest"
+
+describe('Line editor', () => {
+//TODO set anchor on Line editor
+//TODO update anchor when Line update
+//TODO set control point on Line editor
+//TODO update control point when Line update
+//TODO change Line points when dragging anchor
+//TODO add new anchor on editor when dragging control point
+//TODO remove anchor when double click anchor
+})
