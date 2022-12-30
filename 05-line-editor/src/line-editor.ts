76dc90d6173c02eee78dbc878492b6c8445177eb
qiubaisen <abosen@qq.com>
2022-12-30 16:11:36 +0800

    feat: update anchor point

--- 05-line-editor/src/line-editor.ts
+++ 05-line-editor/src/line-editor.ts
@@ -1,11 +1,24 @@
 import Konva from "konva";
 
 export class LineEditor extends Konva.Group {
+    private line?: Konva.Line
 
     attach(line: Konva.Line) {
         let points = line.points();
         for (let i = 0; i < points.length / 2; i++) {
             this.add(new Konva.Circle({name: `${i}-anchor`, radius: 10, x: points[i * 2], y: points[i * 2 + 1]}));
+        }
+
+        this.line = line;
+        line.on('pointsChange', () => {
+            this.update();
+        });
+    }
+
+    private update() {
+        let points = this.line!.points();
+        for (let i = 0; i < points.length / 2; i++) {
+            this.findOne(`.${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
         }
     }
 }
--- 05-line-editor/test/line-editor.test.ts
+++ 05-line-editor/test/line-editor.test.ts
@@ -3,7 +3,6 @@ import Konva from "konva";
 import {LineEditor} from "../src/line-editor";
 
 describe('Line editor', () => {
-    //TODO set anchor on Line editor
     it('should add anchor to line editor', () => {
         let line = new Konva.Line({points: [10, 10, 30, 30]});
         let editor = new LineEditor();
@@ -13,7 +12,15 @@ describe('Line editor', () => {
         expect(editor.findOne('.1-anchor').getAttrs()).toMatchObject({x: 30, y: 30});
     });
 
-    //TODO update anchor when Line update
+    it('should update anchor position when line update', () => {
+        let line = new Konva.Line({points: [10, 10, 30, 30]});
+        let editor = new LineEditor();
+        editor.attach(line);
+        line.points([40, 40, 60, 60]);
+
+        expect(editor.findOne('.0-anchor').getAttrs()).toMatchObject({x: 40, y: 40});
+        expect(editor.findOne('.1-anchor').getAttrs()).toMatchObject({x: 60, y: 60});
+    });
     //TODO set control point on Line editor
     //TODO update control point when Line update
     //TODO change Line points when dragging anchor
