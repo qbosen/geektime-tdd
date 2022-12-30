2aa82650ca393901c11aaf3e182fa45b4dffb059
qiubaisen <abosen@qq.com>
2022-12-30 16:04:03 +0800

    feat: bind line to editor

--- 05-line-editor/src/line-editor.ts
+++ 05-line-editor/src/line-editor.ts
@@ -1,4 +1,11 @@
 import Konva from "konva";
 
 export class LineEditor extends Konva.Group {
+
+    attach(line: Konva.Line) {
+        let points = line.points();
+        for (let i = 0; i < points.length / 2; i++) {
+            this.add(new Konva.Circle({name: `${i}-anchor`, radius: 10, x: points[i * 2], y: points[i * 2 + 1]}));
+        }
+    }
 }
--- 05-line-editor/test/line-editor.test.ts
+++ 05-line-editor/test/line-editor.test.ts
@@ -1,11 +1,22 @@
-import {describe} from "vitest"
+import {describe, expect, it} from "vitest"
+import Konva from "konva";
+import {LineEditor} from "../src/line-editor";
 
 describe('Line editor', () => {
-//TODO set anchor on Line editor
-//TODO update anchor when Line update
-//TODO set control point on Line editor
-//TODO update control point when Line update
-//TODO change Line points when dragging anchor
-//TODO add new anchor on editor when dragging control point
-//TODO remove anchor when double click anchor
-})
+    //TODO set anchor on Line editor
+    it('should add anchor to line editor', () => {
+        let line = new Konva.Line({points: [10, 10, 30, 30]});
+        let editor = new LineEditor();
+        editor.attach(line);
+
+        expect(editor.findOne('.0-anchor').getAttrs()).toMatchObject({x: 10, y: 10});
+        expect(editor.findOne('.1-anchor').getAttrs()).toMatchObject({x: 30, y: 30});
+    });
+
+    //TODO update anchor when Line update
+    //TODO set control point on Line editor
+    //TODO update control point when Line update
+    //TODO change Line points when dragging anchor
+    //TODO add new anchor on editor when dragging control point
+    //TODO remove anchor when double click anchor
+});
